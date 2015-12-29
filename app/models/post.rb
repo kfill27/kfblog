34c7dfcbfc4971ffc543940515ce3a0cc39743b2
Kyra Fillmore <kfillmore27@gmail.com>
2015-12-29 10:41:06 -0500

    Fixed some styling on index page, fixed a validation error.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 3}
-  validates :body, presence: true
+  validates :content, presence: true
 end
