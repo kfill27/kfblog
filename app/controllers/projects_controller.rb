bd95dbea9a016211bd64626a933cceaf83f885f2
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-19 22:04:56 -0500

    File uploads. But wont render on index, only show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -72,6 +72,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :url, :description)
+      params.require(:project).permit(:name, :url, :project_image, :description)
     end
 end
