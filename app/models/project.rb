c78452d441ae3ceebd3d76f93cf922d78c41b303
Kyra Fillmore <kfillmore27@gmail.com>
2016-01-19 21:56:45 -0500

    Created migration

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ActiveRecord::Base
+  attachment :project_image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160116174453) do
+ActiveRecord::Schema.define(version: 20160120025611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,9 @@ ActiveRecord::Schema.define(version: 20160116174453) do
     t.string   "name"
     t.string   "url"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "project_image_id"
   end
 
   create_table "users", force: :cascade do |t|
